70ef9e1389f7899622f48f77de5d51be258bdd33
Blake Brandon <50201165+blakebrandon-hub@users.noreply.github.com>
2025-07-16 08:27:12 -0400

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import os
 app = Flask(__name__)
 
 # Optional: Set this to your actual Hugging Face API token
-HUGGINGFACE_API_TOKEN = "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" # or replace with your token directly
+HUGGINGFACE_API_TOKEN = os.getenv("HF_TOKEN")
 
 HEADERS = {
     "Authorization": f"Bearer {HUGGINGFACE_API_TOKEN}"
